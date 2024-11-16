c6ae32d87e6ff5c13e85ab50f6f16fdfcc0453a9
Ivan Perez <pumi0751@gmail.com>
2024-11-16 15:46:19 +0000

    Add & Fix: add a button delete for delete contact in your agenda and fixed code

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,20 +1,19 @@
 import React, { useContext } from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { Context } from "../store/appContext.js"
 import { ContactCard } from "../component/contactCard.jsx";
 
 export const Home = () => {
-	 const {store, actions} = useContext(Context)
+	const {store, actions} = useContext(Context)
 	return(
 	<div className="text-center mt-5">
 		{
-			store.contact?.map(contact =><ContactCard key={contact.id} 
+			store.contacts?.map(contact => <ContactCard key={contact.id} 
 				name={contact.name}
 				phone={contact.phone}
 				email={contact.email}
 				address={contact.address}
-				contact={contact.id}
+				contactId={contact.id}
 			/>)
 		}
 	</div>
