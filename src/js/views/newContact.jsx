373884b5565a97f7cc1f4d98ad5c59458494838c
Ivan Perez <pumi0751@gmail.com>
2024-11-16 11:03:15 +0000

    feat: add newContactForm

---
+++ src/js/views/newContact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NewContactForm } from "../component/newContacForm";
+
+
+export const NewContact = () => {
+
+
+    return (
+        <div className="container">
+           <NewContactForm />
+        </div>
+    )
+}
